0b685f793f7db781bacbde0851ff98d657e47b7d
rockomatthews <rob.matthews@me.com>
2018-02-17 12:42:47 -0600

    fixed ESLint issue

--- react-redux-realworld-example-app/src/index.js
+++ react-redux-realworld-example-app/src/index.js
@@ -1,10 +1,42 @@
 import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
 import React from 'react';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE': 
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+}
+
+const store = createStore(reducer);
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
+  
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+  }
+  
   render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' });
     return (
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>ToDos</h1>
+        <div>
+          Learn Redux&nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+            onClick={ onClick } />
+        </div>
+      </div>
     );
   }
 }
