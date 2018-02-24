42d8f62e501494e2aea44378ef059cfb909bde08
rockomatthews <rob.matthews@me.com>
2018-02-24 11:29:26 -0600

    connected redux library to react

--- react-redux-realworld-example-app/src/index.js
+++ react-redux-realworld-example-app/src/index.js
@@ -1,46 +1,22 @@
+import App from './App';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import Provider from 'react-redux';
 import React from 'react';
 import { createStore } from 'redux';
 
-const defaultState = { checked: false };
+const defaultState = {
+
+appName: 'conduit',
+articles: null
+};
 const reducer = function(state = defaultState, action) {
-  switch (action.type) {
-    case 'TOGGLE': 
-      return { ...state, checked: !state.checked };
-  }
   return state;
-}
+};
 
 const store = createStore(reducer);
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = store.getState();
-  }
-  
-  componentWillMount() {
-    store.subscribe(() => this.setState(store.getState()));
-  }
-  
-  render() {
-    const onClick = () => store.dispatch({ type: 'TOGGLE' });
-    return (
-      <div>
-        <h1>ToDos</h1>
-        <div>
-          Learn Redux&nbsp;
-          <input
-            type="checkbox"
-            checked={!!this.state.checked}
-            onClick={ onClick } />
-        </div>
-      </div>
-    );
-  }
-}
-
 ReactDOM.render((
-  <App />
+<Provider store={store}>
+    <App />
+</Provider>
 ), document.getElementById('root'));
