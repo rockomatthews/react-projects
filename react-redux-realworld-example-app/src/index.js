372a2000bb19ff440e9f6ff227467939493218a3
rockomatthews <rob.matthews@me.com>
2018-03-01 06:08:29 -0600

    fixed doubles issue from rewind and am displaying pre-set articles

--- react-redux-realworld-example-app/src/index.js
+++ react-redux-realworld-example-app/src/index.js
@@ -2,18 +2,22 @@ import App from './App';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleware } from './middleware';
 
 const defaultState = {
-
-appName: 'vessel',
-articles: null
+  appName: 'vessel',
+  articles: null
 };
+
 const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'HOME_PAGE_LOADED':
+      return { ...state, articles: action.payload.articles };
+  }
   return state;
 };
-
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 ReactDOM.render((
 <Provider store={store}>
