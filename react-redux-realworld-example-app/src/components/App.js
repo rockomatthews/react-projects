12d8db7fce6f96bb55b8f6396361fbe0af8442fe
rockomatthews <rob.matthews@me.com>
2018-03-18 12:29:40 -0500

    stuck after creating login.  problem recognizing '/' as route.  problem likely lies in error within Home's index.js

--- react-redux-realworld-example-app/src/components/App.js
+++ react-redux-realworld-example-app/src/components/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
 import Header from './Header';
 import Home from './Home';
-
+import React from 'react';
+import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
   appName: state.appName
@@ -14,10 +12,14 @@ class App extends React.Component {
     return (
       <div>
         <Header appName={this.props.appName} />
-        <Home />
+       {this.props.children}
       </div>
     );
   }
 }
+
+App.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default connect(mapStateToProps, () => ({}))(App);
