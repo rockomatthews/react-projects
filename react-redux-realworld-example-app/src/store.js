a5e73a19e4f70d0a5e262b25d99776a828f66d59
Rob Matthews <rob.matthews@me.com>
2018-04-08 22:40:23 -0500

    add articleList reducer

--- react-redux-realworld-example-app/src/store.js
+++ react-redux-realworld-example-app/src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { promiseMiddleware, localStorageMiddleware } from './middleware';
 import article from './reducers/article';
+import articleList from './reducers/articleList';
 import auth from './reducers/auth';
 import common from './reducers/common';
 import home from './reducers/home';
@@ -8,6 +9,7 @@ import settings from './reducers/settings';
 
 const reducer = combineReducers({
   article,
+  articleList,
   auth,
   common,
   home,
