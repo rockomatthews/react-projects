af84410297c5634af761debb5b537db844647061
Rob Matthews <rob.matthews@me.com>
2018-04-10 12:47:40 -0500

    Add favorite articles tab for users

--- react-redux-realworld-example-app/src/index.js
+++ react-redux-realworld-example-app/src/index.js
@@ -8,6 +8,7 @@ import Article from './components/Article';
 import Home from './components/Home';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import ProfileFavorites from './components/ProfileFavorites';
 import Register from './components/Register';
 import Settings from './components/Settings';
 import store from './store';
@@ -23,6 +24,7 @@ ReactDOM.render((
             <Route path="settings" component={Settings} />
             <Route path="article/:id" component={Article} />
             <Route path="@:username" component={Profile} />
+            <Route path="@:username/favorites" component={ProfileFavorites} />
         </Route>
     </Router>
 </Provider>
